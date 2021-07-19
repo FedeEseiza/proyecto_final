7f23d497a260780b8fca145fac8132e53e321c85
Federico <federf32@hotmail.com>
2021-07-19 18:01:10 -0300

    add type script effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,10 @@ sr.reveal(`.title_about, .img-about,
             .description_about, .title-body, .container-type-services, .card-bg-burger, .card-bg-beer, .footer-container`, {
     interval: 200
 })
+// ANIMATION TYPED TEXT
+var typed = new Typed(".typing",{
+    strings: ["19:00", "a", "00:00"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
